e63dae10c6d8c96e15fcb9abd9e45c2e176e591d
Elise Pellegry <98736513+ElisePel@users.noreply.github.com>
2023-04-18 11:51:46 +0200

    Create denoising-algo.py

---
+++ denoising-algo.py
@@ -0,0 +1,11 @@
+def ddpm(x_t, pred_noise, t):
+    alpha_t = np.take(alpha, t)
+    alpha_t_bar = np.take(alpha_bar, t)
+
+    eps_coef = (1 - alpha_t) / (1 - alpha_t_bar) ** .5
+    mean = (1 / (alpha_t ** .5)) * (x_t - eps_coef * pred_noise)
+
+    var = np.take(beta, t)
+    z = np.random.normal(size=x_t.shape)
+
+    return mean + (var ** .5) * z
