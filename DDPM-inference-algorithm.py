72b40b9cc1bb714643b791e7266baa5c1bca2c63
Elise Pellegry <98736513+ElisePel@users.noreply.github.com>
2023-04-18 12:04:55 +0200

    Update DDPM-inference-algorithm.py

--- DDPM-inference-algorithm.py
+++ DDPM-inference-algorithm.py
@@ -1,3 +1,4 @@
+#DDPM inference algorithm
 def ddpm(x_t, pred_noise, t):
     alpha_t = np.take(alpha, t)
     alpha_t_bar = np.take(alpha_bar, t)
@@ -9,3 +10,22 @@ def ddpm(x_t, pred_noise, t):
     z = np.random.normal(size=x_t.shape)
 
     return mean + (var ** .5) * z
+
+# random noise
+x = tf.random.normal((1,32,32,1))
+img_list = []
+img_list.append(np.squeeze(np.squeeze(x, 0),-1))
+
+for i in tqdm(range(timesteps-1)):
+    t = np.expand_dims(np.array(timesteps-i-1, np.int32), 0)
+    pred_noise = unet(x, t)
+    x = ddpm(x, pred_noise, t)
+    img_list.append(np.squeeze(np.squeeze(x, 0),-1))
+
+#     if i % 25==0:
+#         plt.imshow(np.array(np.clip((x[0] + 1) * 127.5, 0, 255), np.uint8))
+#         plt.colorbar()
+#         plt.show()
+
+plt.imshow(imshow(img_lit[timesteps-1])
+plt.show()
